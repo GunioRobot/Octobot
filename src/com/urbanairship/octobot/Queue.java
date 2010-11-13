6b687d8c4a48e1fb0d450542fa3a1b9a6aabf979
Dave King <tildedave@gmail.com>
2010-11-13 10:44:04 -0500

    add tasks field, refactor constructors to avoid duplication

--- src/com/urbanairship/octobot/Queue.java
+++ src/com/urbanairship/octobot/Queue.java
@@ -12,13 +12,11 @@ public class Queue {
     public String username;
     public String password;
     public String vhost;
+	public HashMap<String, String> tasks;
 
     public Queue(String queueType, String queueName, String host, Integer port,
         String username, String password) {
-            this.queueType = queueType.toLowerCase();
-            this.queueName = queueName;
-            this.host = host;
-            this.port = port;
+    		this(queueType, queueName, host, port);
             this.username = username;
             this.password = password;
             this.vhost = "/";
@@ -29,18 +27,19 @@ public class Queue {
             this.queueName = queueName;
             this.host = host;
             this.port = port;
+            this.tasks = new HashMap<String, String>();
     }
 
     public Queue(HashMap<String, Object> config) {
-        this.queueName = (String) config.get("name");
-        this.queueType = ((String) config.get("protocol")).toLowerCase();
-        this.host = (String) config.get("host");
+    	this(((String) config.get("protocol")).toLowerCase(), 
+    		 (String) config.get("name"),
+    		 (String) config.get("host"),
+    		 Integer.parseInt(((Long) config.get("port")).toString()));
+    	
         this.vhost = (String) config.get("vhost");
         this.username = (String) config.get("username");
         this.password = (String) config.get("password");
-
-        if (config.get("port") != null)
-            this.port = Integer.parseInt(((Long) config.get("port")).toString());
+        this.tasks = (HashMap<String, String>) config.get("tasks");
     }
 
 
