8f45f70ad6f54065e9c15176ee0ecfacf5ddea2a
Dave King <tildedave@gmail.com>
2010-11-13 10:46:18 -0500

    initial rewrite to handle task mapping

--- src/com/urbanairship/octobot/TaskExecutor.java
+++ src/com/urbanairship/octobot/TaskExecutor.java
@@ -9,9 +9,17 @@ public class TaskExecutor {
 
     private static final HashMap<String, Method> taskCache =
             new HashMap<String, Method>();
+    private static final HashMap<String, Class> taskmapCache = new HashMap<String, Class>();
+    
+    protected HashMap<String, String> classForTask;
+    
+    public TaskExecutor(Queue queue) {
+    	// add task mapping here
+    	classForTask = new HashMap<String, String>(queue.tasks);
+    }
 
-    @SuppressWarnings("unchecked")
-    public static void execute(String taskName, JSONObject message) 
+	@SuppressWarnings("unchecked")
+    public void execute(String taskName, JSONObject message) 
             throws ClassNotFoundException,
                    NoSuchMethodException,
                    IllegalAccessException,
@@ -22,12 +30,37 @@ public class TaskExecutor {
         if (taskCache.containsKey(taskName)) {
             method = taskCache.get(taskName);
         } else {
-            Class task = Class.forName(taskName);
+            Class task = getClassForTaskName(taskName);
             method = task.getMethod("run", new Class[]{ JSONObject.class });
             taskCache.put(taskName, method);
         }
 
         method.invoke(null, new Object[]{ message });
     }
+
+	private Class getClassForTaskName(String taskName)
+			throws ClassNotFoundException {
+		
+		if (taskmapCache.containsKey(taskName))
+			return taskmapCache.get(taskName);
+		
+		// check task mapping
+		Class task = null;
+		String checkName = null;
+		if (classForTask.containsKey(taskName))
+			checkName = classForTask.get(taskName);
+		else
+			checkName = taskName;
+		
+		try {
+			task = Class.forName(checkName);
+			taskmapCache.put(taskName, task);
+		}
+		catch (ClassNotFoundException e) {
+			task = Class.forName(taskName);
+			taskmapCache.put(taskName, task);
+		}
+		return task;
+	}
 
 }
