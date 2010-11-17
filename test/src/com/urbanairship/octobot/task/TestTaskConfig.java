0cc35d903fcbc44a4fb9227e28b4adbf7ff34fb4
Dave King <tildedave@gmail.com>
2010-11-16 19:22:35 -0500

    added initializer-less test, passes

--- test/src/com/urbanairship/octobot/task/TestTaskConfig.java
+++ test/src/com/urbanairship/octobot/task/TestTaskConfig.java
@@ -2,6 +2,7 @@ package com.urbanairship.octobot.task;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,8 +12,15 @@ import org.jvyaml.YAML;
 
 public class TestTaskConfig {
 
-	@Test
 	@SuppressWarnings("unchecked")
+	private Map<String, Map<String, String>> getTaskMapFromYamlString(
+			String yaml) {
+		// types here are a little annoying to fully specify
+		Map<String, Map<String,String>> taskMap = ((Map) ((Map) YAML.load(yaml)).get("tasks"));
+		return taskMap;
+	}
+
+	@Test
 	public void properlyParsesTaskConfigWithInitializer() {
 		String sampleTask = "com.urbanairship.octobot.task.SampleTask";
 		String sampleInitializer = "com.urbanairship.octobot.task.SampleTaskInitializer";
@@ -24,12 +32,29 @@ public class TestTaskConfig {
 			"   }\n" +
 			"}";
 		
-		Map<String, Map<String,String>> taskMap = ((Map) ((Map) YAML.load(yaml)).get("tasks"));
+		Map<String, Map<String, String>> taskMap = getTaskMapFromYamlString(yaml);
 		
 		HashMap<String, TaskConfig> config = TaskConfig.parse(taskMap);
 		assertFalse(config.get("task1").isStatic());
 		assertEquals(sampleTask, config.get("task1").getTaskClass());
 		assertEquals(sampleInitializer, config.get("task1").getTaskInitializer());
+	}
+
+	@Test
+	public void properlyParsesTaskConfigWithoutInitializer() {
+		String sampleTask = "com.urbanairship.octobot.task.SampleTask";
+		String yaml = "tasks: {\n" +
+		"   task1: {\n" +
+		"      class: " + sampleTask + "\n" +
+		"   }\n" +
+		"}";
+		
+		Map<String, Map<String, String>> taskMap = getTaskMapFromYamlString(yaml);
+		
+		HashMap<String, TaskConfig> config = TaskConfig.parse(taskMap);
+		assertTrue(config.get("task1").isStatic());
+		assertEquals(sampleTask, config.get("task1").getTaskClass());
+		assertEquals(null, config.get("task1").getTaskInitializer());
 	}
 	
 }
