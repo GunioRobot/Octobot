84f33ccd55af0f14f9724f88100c51407e9e4895
Dave King <tildedave@gmail.com>
2010-11-16 20:44:37 -0500

    add clarifying comments to OctobotTask

--- src/com/urbanairship/octobot/OctobotTask.java
+++ src/com/urbanairship/octobot/OctobotTask.java
@@ -2,6 +2,16 @@ package com.urbanairship.octobot;
 
 import org.json.simple.JSONObject;
 
+// An OctobotTask is an object that 
+// has a non-static run method.
+
+// Non-static tasks are initialized by
+// OctobotTaskInitializer objects.
+
+// This will allow task behavior to be unit 
+// tested without depending on any static 
+// variables.
+
 public interface OctobotTask {
 
 	public void run(JSONObject object);
